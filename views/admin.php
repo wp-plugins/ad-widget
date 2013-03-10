14a672f547731649320a09e78a6862a7aa6d86a2
broadstreetads <broadstreetads@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-10 13:49:14 +0000

    Fixes readme description
    git-svn-id: https://plugins.svn.wordpress.org/ad-widget/tags/2.2.1@679120 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/admin.php
@@ -0,0 +1,32 @@
+<div style="width: 650px;">
+
+<h1>Wordpress Ad Widget</h1>
+
+<script type="text/javascript" src="http://cdn.broadstreetads.com/init.js"></script>
+<script type="text/javascript">broadstreet.zone(423);</script>
+
+<p>Thank you for using our plugin! This plugin was built by <a href="http://broadstreetads.com">Broadstreet</a>, the
+company for independent publishers.</p>
+
+<p>If you're a local publisher, please <strong><a href="http://broadstreetads.us5.list-manage.com/subscribe?u=508de363fda633d674a0a123b&id=e2fbcef169">sign up for our newsletter</a></strong>, and join the community.</p>
+
+<p>To put an ad on your website, just go to <a href="widgets.php">the widgets page</a>, 
+    and look for:</p>
+
+<ul>
+    <li><strong>Ad: HTML/Javascript Ad</strong></li>
+    <li><strong>Ad: Image/Banner Ad</strong></li>
+</ul>
+
+<p>
+Drag one of those into your a widget area, like a sidebar, and follow the
+instructions! Send questions to kenny@broadstreetads.com .
+</p>
+
+<p>
+Here's a short instructional video if you need help:
+</p>
+
+<iframe src="http://www.screenr.com/embed/u0t7" width="650" height="396" frameborder="0"></iframe>
+
+</div>
