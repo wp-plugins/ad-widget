7033efc17ba7ba52012e4a80c067e811c16137ad
broadstreetads <broadstreetads@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-07 01:39:47 +0000

    Added demo video of WP Ad Widget
    git-svn-id: https://plugins.svn.wordpress.org/ad-widget/trunk@752635 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/modal/signup.php
+++ views/modal/signup.php
@@ -18,11 +18,6 @@
                 </tr>
             </table>
         </div>
-        <div id="info">
-            <h2>You will be able to see <strong>reports on ad views and clicks</strong>. And best of all,
-                ads will be served using
-                <a target="_blank" href="http://broadstreetads.com">Broadstreet Ads</a>' ultra-fast adserver.</h2>    
-        </div>
         <div id="call-to-action">
             <form id="signup" action="?step=signup&status=agree" method="post">
                 <?php if(!Broadstreet_Mini_Utility::getNetworkID()): ?>
@@ -33,5 +28,11 @@
                 <a href="#" onclick="$('#signup').submit();" class="btn call-to-action">$5 / month. Click to re-subscribe.</a>
                 <?php endif; ?>
             </form>
+        </div>
+        <div id="info">
+            <h2>Or, watch a demo first (and <a href="http://broadstreetads.com">check out our website</a>)</h2>    
+        </div>
+        <div style="width: 660px; margin: 0 auto 22px auto;">
+            <iframe width="660" height="370" src="//www.youtube.com/embed/yAFFhjWnOgk" frameborder="0" allowfullscreen></iframe>
         </div>
 <?php require 'footer.php' ?>
